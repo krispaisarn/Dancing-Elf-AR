d6cf99bfaefae2ac0d951b8103289b939af95329
Kris Paisarn <kris.paisarn@gmail.com>
2021-01-19 06:18:19 +0700

    Add UIComponent as ScriptableObject
    * Add scriptableObject UIComponent to stylize UI elements

---
+++ Assets/Scripts/UIComponent.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UIComponent", menuName = "UI Component", order = 51)]
+public class UIComponent : ScriptableObject
+{
+    [SerializeField] string id;
+    [SerializeField] float width;
+    [SerializeField] float height;
+    [SerializeField] Sprite icon;
+    [SerializeField] Color color;
+    [SerializeField] Color backgroundColor;
+
+    public string ID
+    {
+        get
+        {
+            return id;
+        }
+    }
+    public float Width
+    {
+        get
+        {
+            return width;
+        }
+    }
+    public float Height
+    {
+        get
+        {
+            return height;
+        }
+    }
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+    public Color Color
+    {
+        get
+        {
+            return color;
+        }
+    }
+    public Color BackgroundColor
+    {
+        get
+        {
+            return backgroundColor;
+        }
+    }
+}
