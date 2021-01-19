f1f79991c7bd5a56dd460f9276ec6789b0cbf73b
Kris Paisarn <kris.paisarn@gmail.com>
2021-01-19 22:33:39 +0700

    Add PhotoCapture
    * Add Native Gallery for Photo and Video capture
    * Add camera capture button

--- Assets/Scripts/AppControl.cs
+++ Assets/Scripts/AppControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,10 @@ public class AppControl : MonoBehaviour
     [SerializeField]
     private Transform _effects;
     // Start is called before the first frame update
+
+    [SerializeField]
+    private Camera _arCamera;
+
     void Start()
     {
 
@@ -31,6 +36,33 @@ public class AppControl : MonoBehaviour
         {
             t.gameObject.SetActive(false);
         }
+    }
+
+    public void Capture()
+    {
+
+        Texture2D image = RTImage(_arCamera);
+
+
+        NativeGallery.SaveImageToGallery(image, "Dancing Elf AR", "elf_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".jpg", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+
+    }
+
+    Texture2D RTImage(Camera camera)
+    {
+        int resWidth = Screen.width;
+        int resHeight = Screen.height;
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        _arCamera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        _arCamera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        _arCamera.targetTexture = null;
+        RenderTexture.active = null; // JC: a3dded to avoid errors
+        Destroy(rt);
+
+        return screenShot;
     }
 
 }
